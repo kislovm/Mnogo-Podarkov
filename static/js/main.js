4a82105ab2e332bd18c4341f083cde0b0ba70950
Alexander Makaev <alexrule7@gmail.com>
2013-07-16 00:06:22 +0400

    [+] Login pop-up at order-form.html.html (Я уже зарегистрирован) [+] Register pop-up at index.html (Решистрация) [+] Cart and order page

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,16 @@
 $(function() {
     $('.product-mini-holder:nth-of-type(4n)').addClass('ie-nth-child'); // IE last el in the row fallback support
+
+    // Show popup
+    $('.show-popup').click(function(event){
+        event.preventDefault();
+        $('.popup-container').addClass('showing');
+        $('html').css({'overflow':'hidden'})
+    });
+    // Hide popup
+    $('.hide-popup').click(function(event){
+        event.preventDefault();
+        $('.popup-container').removeClass('showing');
+        $('html').css({'overflow':''})
+    });
 });
